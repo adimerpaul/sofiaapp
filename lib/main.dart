646601d913e126918262215bc09fcc801c62c0f0
adimerpaul <adimer101@gmail.com>
2024-02-15 05:09:32 -0400

    funcionando la recuperacion de datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:appsofia/pages/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,19 +11,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Define tu color personalizado
-    final Color myCustomColor = Color(0xFFED1C24); // Ejemplo: Verde
+    final Color myCustomColor = Color(0xFFED1C24);
 
     return MaterialApp(
       title: 'Sofia App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // Establece el color personalizado en el tema de la aplicación
         appBarTheme: AppBarTheme(
           color: myCustomColor,
         ),
       ),
       home: HomePage(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', 'ES'),
+      ],
     );
   }
 }
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _fecha = GlobalKey<FormBuilderState>();
+  final _codigo = GlobalKey<FormBuilderState>(); //['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +21,43 @@ class _HomePageState extends State<HomePage> {
             'Importar y Datos',
             style: TextStyle(
               color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Welcome to the Home Page'),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 100,
+            width: 100,
+            child: Image.asset('logo.png')
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child:  FormBuilderDateTimePicker(
+              name: 'date',
+              key: _fecha,
+              inputType: InputType.date,
+              decoration: InputDecoration(labelText: 'Fecha'),
+              initialValue: DateTime.now(),
+              locale: const Locale('es', 'ES'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: FormBuilderDropdown(
+              key: _codigo,
+              name: 'codigo',
+              decoration: InputDecoration(labelText: 'Código'),
+              items: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+                .map((codigo) => DropdownMenuItem(
+                  value: codigo,
+                  child: Text('$codigo'),
+                )).toList(),
+            ),
+          )
+        ]
       ),
     );
   }
